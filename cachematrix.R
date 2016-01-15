8dadb141d238302c52496206087253f025d0ad6b
JoaquinRV <JdotR@yahoo.com>
2016-01-15 03:24:38 -0800

    Test run of the functions makeCacheMatrix and cacheSolve
    The following is a copy of the console showing test run of the functions.  Note the messages generated.
    > my_matrix = matrix(rnorm(64), 8, 8)
    > my_method1 = makeCacheMatrix(my_matrix)
    > my_invers1 = cacheSolve(my_method1)
    Matrix is verified correct
    inverse calculated
    > my_invers1
                [,1]        [,2]       [,3]       [,4]        [,5]        [,6]
    [1,]  0.60718118  0.46298841  0.5105426 -0.1859946 -0.07951283 -0.04539932
    [2,]  0.68730715  0.17182559  0.4361325  0.2912701 -0.31004390  0.09242869
    [3,] -0.03819975  0.23430485  0.6249172 -0.1746226  0.15326976 -0.11901861
    [4,]  0.26451743 -0.11334632  0.2242546  0.2327712 -0.06132648  0.04723617
    [5,] -0.02034002 -0.18476291 -0.1199376 -0.3410858 -0.27937178  0.37028369
    [6,]  0.02623382 -0.08649885  0.1493254 -0.3878679  0.28085772  0.04611876
    [7,]  0.59006773  0.10810970  0.1684816 -0.2409506 -0.18910437  0.22296135
    [8,] -1.24315649  0.11139153 -0.3255111 -0.2196476  0.85042656 -0.03188156
                [,7]        [,8]
    [1,]  0.26934158 -0.50414069
    [2,] -0.31876481 -0.24237580
    [3,]  0.05048771 -0.21568471
    [4,] -0.11230787 -0.28793179
    [5,]  0.01869086 -0.08983055
    [6,]  0.06363343  0.24053304
    [7,] -0.30760668 -0.35476959
    [8,]  0.51511302  0.63961336
    > 
    > my_invers2 = cacheSolve(my_method1)
    Matrix is verified correct
    inverse retrieved
    > my_invers2
                [,1]        [,2]       [,3]       [,4]        [,5]        [,6]
    [1,]  0.60718118  0.46298841  0.5105426 -0.1859946 -0.07951283 -0.04539932
    [2,]  0.68730715  0.17182559  0.4361325  0.2912701 -0.31004390  0.09242869
    [3,] -0.03819975  0.23430485  0.6249172 -0.1746226  0.15326976 -0.11901861
    [4,]  0.26451743 -0.11334632  0.2242546  0.2327712 -0.06132648  0.04723617
    [5,] -0.02034002 -0.18476291 -0.1199376 -0.3410858 -0.27937178  0.37028369
    [6,]  0.02623382 -0.08649885  0.1493254 -0.3878679  0.28085772  0.04611876
    [7,]  0.59006773  0.10810970  0.1684816 -0.2409506 -0.18910437  0.22296135
    [8,] -1.24315649  0.11139153 -0.3255111 -0.2196476  0.85042656 -0.03188156
                [,7]        [,8]
    [1,]  0.26934158 -0.50414069
    [2,] -0.31876481 -0.24237580
    [3,]  0.05048771 -0.21568471
    [4,] -0.11230787 -0.28793179
    [5,]  0.01869086 -0.08983055
    [6,]  0.06363343  0.24053304
    [7,] -0.30760668 -0.35476959
    [8,]  0.51511302  0.63961336
    >
    > ## the following is to check the calculated and the retrieved matrix by using the function "identical" 
    > identical(my_invers1, my_invers2)
    [1] TRUE
    > 
    >> ## the following is passing an invalid matrix
    > err_matrix = c("a", "b", "c", 1, 2, 3)
    > err_method1 = makeCacheMatrix(err_matrix)
    > err_invers1 = cacheSolve(err_method1)
    Msg#1:  not a matrix
    Msg#2:  not numeric
    Correct the error
    > 
    > ## the following is passing matrix that is not invertible
    > err_matrix2x3 = matrix(c(1, 2, 3, 4, 5, 6), 2, 3)
    > err_method2 = makeCacheMatrix(err_matrix2x3)
    > err_invers2 = cacheSolve(err_method2)
    Msg#3: not a square matrix
    Correct the error
    >

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,86 @@
-## Put comments here that give an overall description of what your
-## functions do
+## There are two R files for Programming Assignment2 and are as follows:
+## 1.  makeCacheMatrix.R -- set various functions whose primary input is
+##                          a valid matrix.  These functions are:
+##      a.  ver_mat -- validate a matrix, if it is numeric, and if it is a square matrix
+##      b.  get_mat -- retrieve the matrix as initially passed as an argument of makeCacheMatrix function
+##      c.  set_mat -- once the matrix is validated, it is stored in an environment for permanency
+##      d.  get_inv -- retrieve the inverse matrix from cache storage
+##      e.  set_inv -- the calculated inverse matrix is stored in an environment for permanency
+## 2.  cacheSolve.R -- the purpose of this function is to calculate the inverse matrix or 
+##                     if it was previously calculated, it retrieves the inverse matrix from cache.
 
-## Write a short comment describing this function
+## makeCacheMatrix.R
+## Purpose:  to define various functions as described above for a valid matrix.
+##           (see above description of the methods created related to the matrix passed
+##            as an argument to makeCacheMatrix.R)
 
-makeCacheMatrix <- function(x = matrix()) {
 
-}
+makeCacheMatrix = function(mat = matrix())
+   {
+      cached_inv = NULL
+      ver_mat = function(mat)
+                {
+                   flg = NULL
+                   if(!is.matrix(mat))
+                      { message("Msg#1:  not a matrix")
+                        flg = 1
+                      }
+                   if(!is.numeric(mat))
+                      { message("Msg#2:  not numeric")
+                        flg = 2
+                      }
+                   if(is.null(flg))
+                      {
+                        if(dim(mat)[1] != dim(mat)[2])
+                          {
+                            message("Msg#3: not a square matrix")
+                            flg = 3
+                          }
+                      }
+                   flg
+                }
+      get_mat = function() mat
+      set_mat = function(mat)
+                {
+                   cached_mat <<- mat
+                   cached_inv <<- NULL
+                }
+      get_inv = function() cached_inv
+      set_inv = function(inv)
+                {
+                   cached_inv <<- inv
+                }
+      list(ver_mat = ver_mat,
+           get_mat = get_mat,
+           set_mat = set_mat,
+           get_inv = get_inv,
+           set_inv = set_inv)
+   }
 
 
-## Write a short comment describing this function
+## cacheSolve.R
+## Purpose:  this function simply calculates the inverse matrix or if it is already
+##           in cache storage, the inverse matrix is retrieved.
 
-cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+
+cacheSolve = function(x, ...)
+             {
+                data_mat = x$get_mat()
+                err = x$ver_mat(data_mat)
+                if(!is.null(err))
+                   { message("Correct the error")
+                     return()
+                   }
+                message("Matrix is verified correct")
+
+                data_inv = x$get_inv()
+                if(!is.null(data_inv))
+                   { message("inverse retrieved")
+                     return(data_inv)
+                   }
+                x$set_mat(data_mat)
+                data_inv = solve(data_mat)
+                message("inverse calculated")
+                x$set_inv(data_inv)
+                return(data_inv)
+             }
